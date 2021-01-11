5c07c2fd4ae3f18efb751df50312d966e8d1ccb4
Afton Slone <afton.slone@gmail.com>
2021-01-11 02:46:46 -0500

    Finihshed MVP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import Form from "./componets/Form";
+import { useState } from "react";
 
 function App() {
   const [team, setTeam] = useState([]);
 
+  const initialFormValues = {
+    name: "",
+    email: "",
+    role: "",
+  };
   const [formValues, setFormValues] = useState(initialFormValues);
 
   const updateForm = (inputName, inputValue) => {
@@ -11,13 +17,38 @@ function App() {
   };
 
   const submitForm = () => {
+    let uniqueName = true;
     const newTeamMember = {
       name: formValues.name.trim(),
       email: formValues.email.trim(),
       role: formValues.role,
+      id: Date.now(),
     };
+    if (!newTeamMember.name || !newTeamMember.email || !newTeamMember.role)
+      return;
+    team.forEach((name) => {
+      if (name.name === newTeamMember.name) {
+        uniqueName = false;
+      }
+    });
+
+    if (uniqueName) {
+      setTeam([...team, newTeamMember]);
+    }
+    setFormValues(initialFormValues);
   };
-  return <Form />;
+  return (
+    <div>
+      {team.map((teamMember) => {
+        return <p key={teamMember.id}>{JSON.stringify(teamMember)}</p>;
+      })}
+      <Form
+        updateForm={updateForm}
+        submitForm={submitForm}
+        formValues={formValues}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/componets/Form.js
+++ src/componets/Form.js
@@ -1,28 +1,55 @@
-export default function Form() {
+export default function Form(proops) {
+  const { updateForm, submitForm, formValues } = proops;
+
   const onChange = (e) => {
     const { name, value } = e.target;
-    update(name, value);
+    updateForm(name, value);
   };
 
   const onSubmit = (e) => {
+    console.log(e);
     e.preventDefault();
-    onSubmit();
+    submitForm();
   };
 
   return (
-    <form className="form container" onSubmit={onSubmit}>
-      <div className="form-group inputs">
+    <form onSubmit={onSubmit}>
+      <div>
         <label>
           Name:
           <input
             type="text"
             name="name"
-            value={null}
+            value={formValues.name}
             onChange={onChange}
             placeholder="Type Your Name"
           />
         </label>
+        <div>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formValues.email}
+              onChange={onChange}
+              placeholder="example@email.com"
+            />
+          </label>
+          <div>
+            <label>
+              Role:
+              <select name="role" value={formValues.role} onChange={onChange}>
+                <option value="">---Select Role---</option>
+                <option value="student">Student</option>
+                <option value="teacher">Teacher</option>
+                <option value="tl">TL</option>
+              </select>
+            </label>
+          </div>
+        </div>
       </div>
+      <button>Submit</button>
     </form>
   );
 }
